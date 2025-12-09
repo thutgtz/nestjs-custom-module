b903e528e71250b246a79ffcb451440aadc1fb6a
ThuT <thut@ThuTs-MacBook-Air-2.local>
2025-12-09 19:12:54 +0700

    chore: prepare release v1.0.4

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
 // Main exports
-export { CustomLoggerModule } from './custom-logger.module'
-export { CustomLogger } from './custom-logger'
+export { CustomLoggerModule } from "./custom-logger.module";
+export { HttpModule } from "./custom-http.module";
+export { CustomLogger } from "./custom-logger";
 
 // Models
-export { LogModel } from './models/log.model'
+export { LogModel } from "./models/log.model";
 
 // Interfaces
-export { LoggerModuleOptions, LoggerModuleAsyncOptions } from './interfaces/logger-options.interface'
+export {
+  LoggerModuleOptions,
+  LoggerModuleAsyncOptions,
+} from "./interfaces/logger-options.interface";
 
 // Constants
-export { LOGGER_MODULE_OPTIONS, DEFAULT_LOG_LEVEL, DEFAULT_SERVICE_NAME, DEFAULT_ENVIRONMENT } from './constants/logger.constants'
+export {
+  LOGGER_MODULE_OPTIONS,
+  DEFAULT_LOG_LEVEL,
+  DEFAULT_SERVICE_NAME,
+  DEFAULT_ENVIRONMENT,
+} from "./constants/logger.constants";
 
 // Middleware - correlation tracking (required for logger to work)
 export {
@@ -20,18 +29,32 @@ export {
   getRequestContext,
   CORRELATION_ID_HEADER,
   RequestContext,
-} from './middleware/correlation.middleware'
+} from "./middleware/correlation.middleware";
 
 // Filters
-export { AllExceptionsFilter, ExceptionNotifyService } from './filters/exception.filter'
+export {
+  AllExceptionsFilter,
+  ExceptionNotifyService,
+} from "./filters/exception.filter";
 
 // Interceptors
-export { LoggingInterceptor } from './interceptors/logging.interceptor'
-export { CustomResponseInterceptor } from './interceptors/response.interceptor'
-export { CustomResponse, BussinessException, ResponseStatusCode } from './interceptors/models/custom-response.model'
+export { LoggingInterceptor } from "./interceptors/logging.interceptor";
+export { CustomResponseInterceptor } from "./interceptors/response.interceptor";
+export {
+  CustomResponse,
+  BussinessException,
+  ResponseStatusCode,
+} from "./interceptors/models/custom-response.model";
 
 // Decorators
-export { SwaggerApiResponse } from './decorators/response-swagger-doc.decorator'
+export { SwaggerApiResponse } from "./decorators/response-swagger-doc.decorator";
 
 // Utilities
-export { sanitizePayload, safeStringify, maskSensitiveData, DEFAULT_SENSITIVE_FIELDS, DEFAULT_MASK_PATTERN, DEFAULT_MAX_BODY_LENGTH } from './utils/sanitizer.util'
+export {
+  sanitizePayload,
+  safeStringify,
+  maskSensitiveData,
+  DEFAULT_SENSITIVE_FIELDS,
+  DEFAULT_MASK_PATTERN,
+  DEFAULT_MAX_BODY_LENGTH,
+} from "./utils/sanitizer.util";
